6f36f0ba86b0d11c0a85cb75eb5ed2c45ed2c3ce
Cyro Gund <43975247+CyroGund@users.noreply.github.com>
2019-02-20 20:22:33 -0300

    Add files via upload

---
+++ checkage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace checkage
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Process ();
+            Console.ReadKey();
+
+        }
+
+        private static void Process()
+        {
+            int Age;
+            Console.WriteLine("Inform your age: ");
+            int.TryParse(Console.ReadLine(), out Age);
+
+            if ( Age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Access Allowed");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Access Denied");
+            }
+        }
+    }
+}
